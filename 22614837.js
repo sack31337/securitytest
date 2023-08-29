adbaff9fdbf5e2c88f2f5a09d347bf8f10a5390e
Howard Shin <hshin@coursera.org>
2023-08-29 10:23:44 -0700

    Delete Old pjt files

--- 22614837.js
+++
@@ -1,7 +0,0 @@
-// HubSpot Script Loader. Please do not block this resource. See more: http://hubs.ly/H0702_H0
-
-!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://js.hscollectedforms.net/collectedforms.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("CollectedForms-22614837",0,{"crossorigin":"anonymous","data-leadin-portal-id":22614837,"data-leadin-env":"prod","data-loader":"hs-scriptloader","data-hsjs-portal":22614837,"data-hsjs-env":"prod","data-hsjs-hublet":"na1"});
-!function(e,t){if(!document.getElementById(e)){var c=document.createElement("script");c.src="https://js.hs-analytics.net/analytics/1666820400000/22614837.js",c.type="text/javascript",c.id=e;var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(c,n)}}("hs-analytics");
-var _hsp = window._hsp = window._hsp || [];
- _hsp.push(['addEnabledFeatureGates', []]);
-!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://js.hs-banner.com/22614837.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("cookieBanner-22614837",0,{"data-cookieconsent":"ignore","data-hs-ignore":true,"data-loader":"hs-scriptloader","data-hsjs-portal":22614837,"data-hsjs-env":"prod","data-hsjs-hublet":"na1"});
